16ad72019b540200543fbf93e7ff3e1faf06d88c
Itzik Galanti <109910835+IZ1KG@users.noreply.github.com>
2025-07-25 16:29:14 +0300

    Update main.tf
    adding automated installation of MVN and JENKINS

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
+//Using this region to deploy freely t2.micro
 provider "aws" {
-  // For free use of t2.micro instances in the free tier.
   region = "us-east-1"
 }
 
@@ -41,20 +41,26 @@ resource "aws_security_group" "app_sg" {
 }
 
 resource "aws_instance" "app_ec2" {
-  ami                    = "ami-0fc5d935ebf8bc3bc"
+  ami                    = "ami-0fc5d935ebf8bc3bc"  # Ubuntu 22.04 in us-east-1
   instance_type          = "t2.micro"
   key_name               = "itzik-key"
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   user_data = <<-EOF
-    #!/bin/bash
-    apt-get update -y && \
-    apt-get install -y docker.io && \
-    systemctl start docker && \
-    systemctl enable docker && \
-    docker pull itzikgalanti/deployment-demo:latest && \
-    docker run -d -p 80:8080 itzikgalanti/deployment-demo
-  EOF
+              #!/bin/bash
+              apt-get update -y && \
+              apt-get install -y openjdk-17-jdk maven docker.io curl gnupg && \
+              systemctl start docker && \
+              systemctl enable docker && \
+              docker pull itzikgalanti/deployment-demo:latest && \
+              docker run -d -p 80:8080 itzikgalanti/deployment-demo && \
+              curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null && \
+              echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | tee /etc/apt/sources.list.d/jenkins.list > /dev/null && \
+              apt-get update -y && \
+              apt-get install -y jenkins && \
+              systemctl start jenkins && \
+              systemctl enable jenkins
+            EOF
 
   tags = {
     Name = "Dev-Task"
