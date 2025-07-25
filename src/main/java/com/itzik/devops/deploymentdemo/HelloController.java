39d4d50a2a86aab7d9dc52a5e44dedd293a674f4
Itzik galanti <itzikga@gmail.com>
2025-07-25 19:59:19 +0300

    CI/CD test 5

--- src/main/java/com/itzik/devops/deploymentdemo/HelloController.java
+++ src/main/java/com/itzik/devops/deploymentdemo/HelloController.java
@@ -30,7 +30,7 @@ public class HelloController {
                     </style>
                 </head>
                 <body>
-                    <h1>Hello from Itzik Galanti 🚀 <br> Hope  will like 😁</h1>
+                    <h1>Hello from Itzik Galanti 🚀 <br> Hope  Uwill like 😁</h1>
                 </body>
             </html>
             """;
